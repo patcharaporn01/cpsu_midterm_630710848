dfde50628dbba9b188b9bbb91fc8c031ee65d62f
patcharaporn01 <phatcharapornchimwat@gmail.com>
2023-09-03 12:08:31 +0700

    aaaa

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
 
-// TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = 'Student ID';
+const studentId = '630710848';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,7 +12,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var textTheme = Theme.of(context).textTheme;
+    var textTheme = Theme
+        .of(context)
+        .textTheme;
 
     return Scaffold(
       body: SafeArea(
@@ -40,11 +39,7 @@ class _HomePageState extends State<HomePage> {
                   textAlign: TextAlign.center,
                   style: textTheme.headlineSmall!.copyWith(
                       fontWeight: FontWeight.bold, color: Colors.black87)),
-              Spacer(),
-              _buildQuizView(),
-              Spacer(),
-              _buildButtonPanel(),
-              SizedBox(height: 16.0),
+              _buildAdditionalContent(),
             ],
           ),
         ),
@@ -52,13 +47,181 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _buildQuizView() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+  Widget _buildAdditionalContent() {
+    return Container(
+      width: 200.0,
+      height: 500.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'Question 30 of 30',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(30.0),
+              border: Border.all(color: Colors.black, width: 2.0),
+            ),
+            width: 400.0,
+            height: 80.0,
+            padding: EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                'What is the longest river in  the world?',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            width: 400.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                'Amazon River',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            width: 400.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                'Nile River',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            width: 400.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                'Mississippi River',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            width: 400.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                'Yangtze River',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+          _buildQuizView(),
+          SizedBox(height: 16.0),
+          //_buildButtonPanel(),
+        ],
+      ),
+    );
   }
 
-  _buildButtonPanel() {
+  _buildQuizView() {
     // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(color: Colors.black),
+              backgroundColor: Colors.redAccent,
+            ),
+            child: Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+              child: Text(
+                '<',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(color: Colors.black),
+              backgroundColor: Colors.lightGreenAccent,
+            ),
+            child: Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical:10.0, horizontal: 60.0),
+              child: Text(
+                '>',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
